5a6a0a67bae407a4e8d25a83087e2881b375ea96
yrmint <zhilkinal@yandex.ru>
2026-04-26 19:55:55 +0300

    feat: prediction api endpoint using mock data

---
+++ backend/app/api/routers/predict_router.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter, File, HTTPException, UploadFile
+
+from app.models.prediction_model import PredictionResponse, Top3Item
+from app.services.genre_classifier import GenreClassifier
+
+router = APIRouter(
+    prefix="/predict",
+    tags=["Prediction"]
+)
+classifier = GenreClassifier()
+
+
+@router.post("/", response_model=PredictionResponse)
+async def predict_genre(audio_file: UploadFile = File(...)):
+    """
+    Receives an audio file and returns predicted genre + top-3.
+    :param audio_file: audio file to be classified
+    """
+    if not (audio_file.filename.lower().
+            endswith(('.mp3', '.wav', '.flac', '.ogg'))):
+        raise HTTPException(
+            status_code=415,
+            detail="Only supported formats: mp3, wav, flac, ogg")
+
+    try:
+        # call audio preprocessor later, for now - just read file to memory
+        audio_bytes = await audio_file.read()
+
+        if len(audio_bytes) == 0:
+            raise HTTPException(
+                status_code=422,
+                detail="Uploaded audio file is empty")
+
+        # placeholder, pass tensor later
+        predicted_genre, confidence, top_3 = classifier.predict(None)
+
+        return PredictionResponse(
+            predicted_genre=predicted_genre,
+            confidence=confidence,
+            top_3=[Top3Item(**item) for item in top_3],
+        )
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Audio processing error: {e}")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
-from app.api.routers import health_router, root_router
+from app.api.routers.root_router import router as root_router
+from app.api.routers.health_router import router as health_router
+from app.api.routers.predict_router import router as predict_router
 
 app = FastAPI()
 
-app.include_router(health_router.router)
-app.include_router(root_router.router)
+app.include_router(root_router)
+app.include_router(health_router)
+app.include_router(predict_router)
